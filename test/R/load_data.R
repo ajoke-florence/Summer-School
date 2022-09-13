940cbf3508d6ba131d924c84ee066d1eae9f5f25
ajoke-florence <florence.kayode@aims.ac.rw>
2022-09-13 15:16:10 +0000

    update

--- test/R/load_data.R
+++ test/R/load_data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-
+setwd('test/R/')
 penguin_csv_url <- "https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv"
 
 penguins_online <- read_csv(penguin_csv_url)
+
+penguin_local <- read_csv('penguin2.csv')
